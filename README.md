73ca3316e9a1d8a8d562c6c140544d588fc83aff
Jahnvigupta9005 <110279662+Jahnvigupta9005@users.noreply.github.com>
2023-05-21 13:15:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# AI-News-Classifier
+
+### An AI classifier for News Headlines
+
+This is a web app that classifies news into 4 categories: Sports, Crime, Science, and Business using a machine learning model trained on a dataset of 40000 news articles.
+
+The model was trained using the [scikit-learn](https://scikit-learn.org/) Python library using a naive Bayes classifier.
+```
---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__, template_folder='Templates/', static_folder='static/')
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/', methods=['GET'])
+def Home():
+    return render_template('index.html', prediction={})
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    my_list = request.form.values()
+    print(my_list)
+    ans = model.predict(my_list)
+    diction={}
+    for index, i in enumerate(ans):
+        if i == 0:
+           diction[index] = "BUSINESS NEWS"
+        elif i == 1:
+           diction[index] = "SPORTS NEWS"
+        elif i == 2:
+            diction[index] = "CRIME NEWS"
+        elif i == 3:
+            diction[index]="SCIENCE NEWS"
+
+
+    return render_template('index.html',prediction = diction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
